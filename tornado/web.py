1694cebc20b964e0b3ef3b81ec23e24a75bdbb65
MinRK <benjaminrk@gmail.com>
2013-08-25 10:11:45 +0200

    Switch temporary redirect default to 307
    It seems that 302 is ambiguous
    and its use discouraged in favor of 303 or 307.

--- tornado/web.py
+++ tornado/web.py
@@ -503,14 +503,14 @@ class RequestHandler(object):
         """Sends a redirect to the given (optionally relative) URL.
 
         If the ``status`` argument is specified, that value is used as the
-        HTTP status code; otherwise either 301 (permanent) or 302
+        HTTP status code; otherwise either 301 (permanent) or 307
         (temporary) is chosen based on the ``permanent`` argument.
-        The default is 302 (temporary).
+        The default is 307 (temporary).
         """
         if self._headers_written:
             raise Exception("Cannot redirect after headers have been written")
         if status is None:
-            status = 301 if permanent else 302
+            status = 301 if permanent else 307
         else:
             assert isinstance(status, int) and 300 <= status <= 399
         self.set_status(status)
